02e5d43b4fbf69f874c3d0584d2320b36ece268d
95zhoujun <37738396+95zhoujun@users.noreply.github.com>
2020-06-16 19:22:54 +0800

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import recommendation
-prefs=recommendation.loadMovieLens()
-#print(prefs['87'])
-#print(recommendation.getRecommendations(prefs,'87')[0:30])
-itemsim = recommendation.calculateSimilarItems(prefs,n=50)
-print(recommendation.getRecommendedItems(prefs,itemsim,'87')[0:30])
