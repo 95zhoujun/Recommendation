0c4e4b50909175710d6afaede4bdf528079c3e5d
95zhoujun <37738396+95zhoujun@users.noreply.github.com>
2020-06-16 19:26:57 +0800

    Create test.py

---
+++ chapter2/test.py
@@ -0,0 +1,7 @@
+from chapter2 import recommendation
+
+prefs= recommendation.loadMovieLens()
+#print(prefs['87'])
+#print(recommendation.getRecommendations(prefs,'87')[0:30])
+itemsim = recommendation.calculateSimilarItems(prefs, n=50)
+print(recommendation.getRecommendedItems(prefs, itemsim, '87')[0:30])
